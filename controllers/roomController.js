531ddd2f4e5072f0fe4b59e5795fdad461d75823
Raleke <116227694+Raleke@users.noreply.github.com>
2025-08-20 15:06:20 +0100

    Updated room controller.js

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,5 +1,4 @@
 const roomService = require("../services/roomService.js");
-
 const roomController = {
   createRoom: async (req, res, next) => {
     try {
@@ -7,47 +6,65 @@ const roomController = {
         ...req.body,
         createdBy: req.user.id
       });
-      return res.status(201).json(room);
+      res.status(201).json(room);
     } catch (err) {
-      return next(err);
+      next(err);
     }
   },
 
   joinRoom: async (req, res, next) => {
     try {
       const room = await roomService.joinRoom(req.params.roomId, req.user.id);
-      return res.json(room);
+      res.json(room);
     } catch (err) {
-      return next(err);
+      next(err);
     }
   },
 
   leaveRoom: async (req, res, next) => {
     try {
       const room = await roomService.leaveRoom(req.params.roomId, req.user.id);
-      return res.json(room);
+      res.json(room);
     } catch (err) {
-      return next(err);
+      next(err);
     }
   },
-
   getRoomDetails: async (req, res, next) => {
     try {
       const room = await roomService.getRoomDetails(req.params.roomId);
       if (!room) return res.status(404).json({ error: "Room not found" });
-      return res.json(room);
+      res.json(room);
     } catch (err) {
-      return next(err);
+      next(err);
     }
   },
 
   assignRole: async (req, res, next) => {
     try {
-      const { userId, role } = req.body;
-      const room = await roomService.assignRole(req.params.roomId, userId, role);
-      return res.json(room);
+      const { targetUserId, role } = req.body;
+      const room = await roomService.assignRole(
+        req.params.roomId,
+        req.user.id,
+        targetUserId,
+        role
+      );
+      res.json(room);
     } catch (err) {
-      return next(err);
+      next(err);
+    }
+  },
+
+  removeMember: async (req, res, next) => {
+    try {
+      const { targetUserId } = req.body;
+      const room = await roomService.removeMember(
+        req.params.roomId,
+        req.user.id,
+        targetUserId
+      );
+      res.json(room);
+    } catch (err) {
+      next(err);
     }
   }
 };
