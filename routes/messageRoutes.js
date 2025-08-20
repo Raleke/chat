57eb0d5ef642b2514f89deae39875caa40dc5023
Raleke <116227694+Raleke@users.noreply.github.com>
2025-08-20 14:12:28 +0100

    messageRoutes .js

---
+++ routes/messageRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { messageController } = require("../controllers/messageController.js");
+const { authMiddleware } = require("../middlewares/authMiddleware.js");
+
+const router = express.Router();
+
+// 💬 Message routes (protected)
+router.post("/", authMiddleware, messageController.sendMessage);
+router.post("/:messageId/read", authMiddleware, messageController.markAsRead);
+router.post("/:messageId/react", authMiddleware, messageController.addReaction);
+router.post("/:messageId/unreact", authMiddleware, messageController.removeReaction);
+
+module.exports = router;
